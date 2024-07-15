27f950d8f40a0158c6021170cb7f39b21566ae08
glasdou.dev <dr.glasdou@gmail.com>
2024-07-15 10:05:14 -0600

    chore: Add Order module and related entities, DTOs, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { RedisModule } from './redis/redis.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [RedisModule, AuthModule, UsersModule, ProductModule],
+  imports: [RedisModule, AuthModule, UsersModule, ProductModule, OrderModule],
 })
 export class AppModule {}
